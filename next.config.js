74c0579920f69aa0fa5ffb7ab4298b240034af23
rodrigox123x <noexxcussess@gmail.com>
2025-08-08 01:05:40 +0100

    Fix: Cambiar next.config.ts a next.config.js para Vercel

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
-  },
-  experimental: {
-    esmExternals: false
   }
 }
 
